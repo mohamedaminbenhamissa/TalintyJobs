3710d5a66bad1062f442470f01d21e04277be063
mohamedaminbenhamissa <mohamedamin.benhamissa@gmail.com>
2024-05-17 16:00:27 +0200

    responsive

--- src/app/[locale]/details/page.tsx
+++ src/app/[locale]/details/page.tsx
@@ -3,9 +3,9 @@ import JobBody from "@/component/jobBody";
 import { JobDetails } from "@/component/jobDetails";
 import JobOverview from "@/component/jobOverview";
 
-import { Box, Button, Grid, Typography } from "@mui/material";
+import { Box, Button, Grid, Stack, Typography } from "@mui/material";
 import PriviousIcon from "../../../../public/assets/priviousIcon";
-
+import Link from "next/link";
 import { useLocale, useTranslations } from "next-intl";
 import { useRouter } from "next/navigation";
 
@@ -17,32 +17,41 @@ export default function Details() {
   const handleNavigate = () => {
     router.push(`/${locale}`);
   };
-
   return (
-    <main className="flex min-h-screen w-full flex-col items-start justify-between">
+    <main className="flex min-h-screen w-full flex-col items-start justify-between ">
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-      <Box width={"100%"} sx={{ p: 2 }}>
-        <Grid container spacing={4} direction="column">
-          <Grid item>
+      <Box width={"100%"}>
+        <Grid spacing={4} direction={"column"}>
+          <Grid display={"flex"} gap={1}>
             <Button style={{ color: "black" }} onClick={handleNavigate}>
               <PriviousIcon />
               {t("backhomebtn")}
             </Button>
           </Grid>
 
-          <Grid item>
+          <Box sx={{ mt: 4 }}>
             <JobDetails />
-          </Grid>
-
-          <Grid item container spacing={2} sx={{ mt: 4 }}>
-            <Grid item xs={12} md={8}>
+          </Box>
+          <Stack
+            direction={"row"}
+            display={"flex"}
+            justifyContent="space-between"
+          >
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                marginLeft: 4,
+                marginRight: 4,
+              }}
+            >
               <JobBody />
-            </Grid>
-            <Grid item xs={12} md={4}>
+            </Box>
+            <Grid item xs={12} md={4} marginRight={4}>
               <JobOverview />
             </Grid>
-          </Grid>
+          </Stack>
         </Grid>
       </Box>
     </main>
